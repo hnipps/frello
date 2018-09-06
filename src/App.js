79715a11906430f15868b33b51bbe6ba1a87a9e8
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-09-06 19:58:05 -0400

    Created basic app layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        {/* Navbar */}
+        <div>
+          <h1>Frello</h1>
+          <button>Boards</button>
+        </div>
+        {/* Board Content */}
+        <div>
+          {/* Board Header */}
+          <div>
+            <h2>Bucket List</h2>
+          </div>
+          {/* List Listing */}
+          <div>
+            <article>
+              <h3>Stuff I wanna do</h3>
+              <ul>
+                <li>Learn to speak Japanese</li>
+                <li>Order salad instead of fries (just once)</li>
+                <li>
+                  <input placeholder="Add a card..." />
+                </li>
+              </ul>
+            </article>
+            <article>
+              <h3>Stuff I've planned</h3>
+              <ul>
+                <li>Ski in Colorado</li>
+                <li>
+                  <input placeholder="Add a card..." />
+                </li>
+              </ul>
+            </article>
+            <article>
+              <h3>Stuff I did!</h3>
+              <ul>
+                <li>Visit Macchu Picchu</li>
+                <li>Eat pizza in Rome</li>
+                <li>
+                  <input placeholder="Add a card..." />
+                </li>
+              </ul>
+            </article>
+            <input placeholder="Add a list..." />
+          </div>
+        </div>
       </div>
     );
   }
