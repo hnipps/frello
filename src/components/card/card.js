e5be9cced0d86a82b349692d9c59619b62aaeb05
Harry Nicholls <harry.m.nicholls@gmail.com>
2019-02-12 17:10:32 -0500

    Replaces Card CSS with Tailwind classes

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import "./card.css";
 
 class Card extends Component {
   render() {
     return (
       <li
-        className="card"
+        className="flex items-center justify-between mb-2 border-solid border-0 border-transparent border rounded bg-grey-lightest px-2"
       >
-        <p className="card-content unselectable">{this.props.cardContent}</p>
+        <p className="text-base my-1 mr-1 text-grey-darkest select-none">{this.props.cardContent}</p>
         <button
-          className="card-delete-button"
+          className="text-xl font-semibold font-sans border-0 py-1 bg-transparent text-grey-darker hover:text-red-dark"
           value={`${this.props.title}-card-${this.props.cardContent}`}
           onClick={this.props.deleteCard}
         >
